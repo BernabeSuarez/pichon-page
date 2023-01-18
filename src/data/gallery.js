7a38045153506dc979c0f16cdf65606c77b4db80
Bernabe <bernatattoo@gmail.com>
2023-01-18 14:05:08 -0300

    Add gallery (provisional)

---
+++ src/data/gallery.js
@@ -0,0 +1,37 @@
+export const dataGallery = [
+    {
+        id: 1,
+        src: "Img/1A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 2,
+        src: "Img/2A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 3,
+        src: "Img/3A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 4,
+        src: "Img/4A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 5,
+        src: "Img/5A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 6,
+        src: "Img/6A.jpg",
+        alt: "Tattoo Photo"
+    },
+    {
+        id: 7,
+        src: "Img/7A.jpg",
+        alt: "Tattoo Photo"
+    },
+]
