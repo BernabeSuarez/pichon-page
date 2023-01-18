f42c6344ca945b37e2c0b36d0212c77ad2e97b17
Bernabe <bernatattoo@gmail.com>
2023-01-18 14:36:56 -0300

    change photos

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,37 +1,37 @@
 export const dataGallery = [
     {
         id: 1,
-        src: "Img/1A.jpg",
+        src: "Img/1A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 2,
-        src: "Img/2A.jpg",
+        src: "Img/2A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 3,
-        src: "Img/3A.jpg",
+        src: "Img/3A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 4,
-        src: "Img/4A.jpg",
+        src: "Img/4A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 5,
-        src: "Img/5A.jpg",
+        src: "Img/5A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 6,
-        src: "Img/6A.jpg",
+        src: "Img/6A.webp",
         alt: "Tattoo Photo"
     },
     {
         id: 7,
-        src: "Img/7A.jpg",
+        src: "Img/7A.webp",
         alt: "Tattoo Photo"
     },
 ]
