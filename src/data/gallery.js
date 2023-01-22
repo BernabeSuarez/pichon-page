e3b2defae4504d8b00bcd0e404551953194f38f9
Bernabe <bernatattoo@gmail.com>
2023-01-22 00:48:00 -0300

    add tailwinds css

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -2,36 +2,43 @@ export const dataGallery = [
     {
         id: 1,
         src: "Img/1A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div1"
     },
     {
         id: 2,
         src: "Img/2A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div2"
     },
     {
         id: 3,
         src: "Img/3A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div3"
     },
     {
         id: 4,
         src: "Img/4A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div4"
     },
     {
         id: 5,
         src: "Img/5A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div5"
     },
     {
         id: 6,
         src: "Img/6A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div6"
     },
     {
         id: 7,
         src: "Img/7A.webp",
-        alt: "Tattoo Photo"
+        alt: "Tattoo Photo",
+        class: ".div7"
     },
 ]
