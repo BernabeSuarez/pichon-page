c308ddc0d8a84072eb8868699cf05810dbd81b59
Bernabe <bernatattoo@gmail.com>
2023-01-25 22:00:31 -0300

    fix images url

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -7,37 +7,37 @@ export const dataGallery = [
     },
     {
         id: 2,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/koi.webp",
+        src: "/img/indio.webp",
         alt: "Tattoo Photo",
         class: ".div2"
     },
     {
         id: 3,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/universo.webp",
+        src: "/img/koi.webp",
         alt: "Tattoo Photo",
         class: ".div3"
     },
     {
         id: 4,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/mariposa.webp",
+        src: "/img/mariposa.webp",
         alt: "Tattoo Photo",
         class: ".div4"
     },
     {
         id: 5,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/indio.webp",
+        src: "/img/universo.webp",
         alt: "Tattoo Photo",
         class: ".div5"
     },
     {
         id: 6,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/pelota.webp",
+        src: "/img/pelota.webp",
         alt: "Tattoo Photo",
         class: ".div6"
     },
     {
         id: 7,
-        src: "https://raw.githubusercontent.com/BernabeSuarez/pichon-page/main/public/Img/momo.webp",
+        src: "/img/momo.webp",
         alt: "Tattoo Photo",
         class: ".div7"
     },
